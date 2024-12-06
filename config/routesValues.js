194d8de9576e381b28bc6bafb71ff372836fd759
EYBhargav <eybhargav@gmail.com>
2024-12-06 13:06:12 +0530

    Added application detail API and count API changes

--- config/routesValues.js
+++ config/routesValues.js
@@ -3,4 +3,6 @@ exports.routeValues = {
   "/common/verify-kyc": "c2a04e70f16541f5b52f1b0803ee46d3",
   "/application/tracker-count": "c2a04e70f16541f5b52f1b0803ee46d3",
   "/application/tracker-list": "c2a04e70f16541f5b52f1b0803ee46d3",
+  "/application/details": "c2a04e70f16541f5b52f1b0803ee46d3",
+  "/application/submission": "c2a04e70f16541f5b52f1b0803ee46d3",
 };
