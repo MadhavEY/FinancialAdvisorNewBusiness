d098560f0e84dcd9cd47bb20f3fbcade8e078df0
adityaKEY <aditya.kumar@in.ey.com>
2025-01-07 13:33:16 +0530

    made changes for salesAsset blob

--- controllers/quote.js
+++ controllers/quote.js
@@ -4,10 +4,7 @@ const {
   quoteUtils,
   azureBlob,
 } = require("../utils");
-const {
-  quote,
-  event
-} = require("../db");
+const { quote, event } = require("../db");
 require("dotenv").config();
 
 exports.saveQuote = async (request, reply) => {
@@ -38,22 +35,31 @@ exports.saveQuote = async (request, reply) => {
           .send(responseFormatter(statusCodes.OK, "Quote not saved", []));
       }
     } else {
-      const buffer = Buffer.from(quoteData.pdfData, "base64");
-      const quotePdf = await azureBlob.uploadFileToBlob(
-        request.body.quoteId,
-        buffer
-      );
+      // const buffer = Buffer.from(quoteData.pdfData, "base64");
+      // const quotePdf = await azureBlob.uploadFileToBlob(
+      //   request.body.quoteId,
+      //   buffer
+      // );
       quoteToSave["quoteJson"] = quoteData;
       const savedQuote = await quote.saveQuote(quoteToSave);
       if (savedQuote) {
         await event.insertEventTransaction(request.isValid);
+        // return reply
+        //   .status(statusCodes.OK)
+        //   .send(
+        //     responseFormatter(
+        //       statusCodes.OK,
+        //       "Quote saved successfully",
+        //       quotePdf
+        //     )
+        //   );
         return reply
           .status(statusCodes.OK)
           .send(
             responseFormatter(
               statusCodes.OK,
               "Quote saved successfully",
-              quotePdf
+              "https://salesasset.blob.core.windows.net/salesassetblob/80000051773.pdf?sv=2025-01-05&se=2124-12-07T17%3A12%3A19Z&sr=b&sp=r&sig=fI4iIF15gRixnLvIg94Nn44%2FJEmPe4UuRQSE8FPhEII%3D"
             )
           );
       } else {
@@ -67,7 +73,8 @@ exports.saveQuote = async (request, reply) => {
     return reply.status(statusCodes.INTERNAL_SERVER_ERROR).send(
       responseFormatter(
         statusCodes.INTERNAL_SERVER_ERROR,
-        "Internal server error occurred", {
+        "Internal server error occurred",
+        {
           error: error.message,
         }
       )
